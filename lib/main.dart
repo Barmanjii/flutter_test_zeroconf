4d84dc19cf174a37bd7b44bcf3eabd1f70c31e10
barman-peppermint <vijay.barman@getpeppermit.co>
2024-02-14 16:03:11 +0530

    first commit & initialised the mDNS to discover the local network

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test_zeroconf/mDNS.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: MyWidget(),
+  ));
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({Key? key}) : super(key: key);
+  void handleClick(BuildContext context) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: const Text('This is a Snackbar'),
+        duration: const Duration(seconds: 2),
+        action: SnackBarAction(
+          label: 'Close',
+          onPressed: () {},
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Show Wifiiiiii !!!!!!!!"),
+      ),
+      body: Align(
+        alignment: Alignment.topLeft,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: ElevatedButton(
+            onPressed: () async {
+              // handleClick(context);
+              await mDNS();
+            },
+            child: const Text("Click Me!!!"),
+          ),
+        ),
+      ),
+    );
+  }
+}
